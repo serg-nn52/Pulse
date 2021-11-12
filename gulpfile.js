64ee0ddc7de68c25658833f5669b6b72b42e4eff
Sergey <serg-nn@inbox.ru>
2021-11-13 01:21:12 +0300

    Сделано 2 первых блока с адаптивом

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp        = require('gulp');
 const browserSync = require('browser-sync');
-const sass        = require('gulp-sass');
+const sass        = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
@@ -31,3 +31,4 @@ gulp.task('watch', function() {
 })
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+  
